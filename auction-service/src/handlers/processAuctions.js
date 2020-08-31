887264a6e1932b12e3ab0c306ccf4e52c5584f7b
Raul Costa <raulbrennersc@gmail.com>
2020-08-31 10:11:37 -0300

    Adicionando funções para pegar auctions a serem fechadas e para fechar essas auctions

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,6 +1,18 @@
+import { getEndedAuctions } from '../lib/getEndedAuctions';
+import { closeAuction } from '../lib/closeAuction';
+import createError from 'http-errors'
+
 async function processAuctions(event, context){
-    console.log('Processing auctions!');
-    
+  try{
+    const auctionsToClose = await getEndedAuctions();
+    const closedPromises = auctionsToClose.map(auction => closeAuction(auction));
+    console.log(auctionsToClose);
+    await Promise.all(closedPromises);
+    return { closed: closedPromises.length };
+  } catch(error){
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
