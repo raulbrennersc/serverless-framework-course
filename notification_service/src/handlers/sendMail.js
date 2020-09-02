d3f641d000ddc6e20e63eeccd917edaf2b10b9dd
Raul Costa <raulbrennersc@gmail.com>
2020-09-02 09:56:55 -0300

    Enviando e-mail de teste

--- notification_service/src/handlers/sendMail.js
+++ notification_service/src/handlers/sendMail.js
@@ -1,10 +1,34 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'sa-east-1'});
+
+async function sendMail(event, context) {
+  const params = {
+    Source: 'raulbrennersc@gmail.com',
+    Destination: {
+      ToAddresses: ['raulbrennersc@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
   };
+
+  try{
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch(error){
+    console.log(error);
+  }
 }
 
-export const handler = hello;
+export const handler = sendMail;
 
 
